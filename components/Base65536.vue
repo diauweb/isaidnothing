c53516558e979175c53f5d2d76d84216dfa536f2
Xiaodai Dai <diauweb@live.com>
2020-03-26 10:28:05 +0800

    Add base65536

---
+++ components/Base65536.vue
@@ -0,0 +1,17 @@
+<script>
+
+import * as Base65536 from '~/node_modules/base65536/dist/es6/base65536.js'
+
+export default {
+  algo: {
+    encode (text) {
+      const uint8 = new TextEncoder('utf-8').encode(text)
+      return Base65536.encode(uint8)
+    },
+    decode (text) {
+      const uint8 = new TextDecoder('utf-8')
+      return uint8.decode(Base65536.decode(text))
+    }
+  }
+}
+</script>
