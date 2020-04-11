c820b738894b566b4cbe324379803ebe22b4094d
Xiaodai Dai <diauweb@live.com>
2020-04-11 15:25:11 +0800

    fix: base64 unicode

--- components/Base64.vue
+++ components/Base64.vue
@@ -1,11 +1,13 @@
 <script>
+import Base64 from '../assets/Base64.js'
+
 export default {
   algo: {
     encode (text) {
-      return btoa(text)
+      return Base64.encode(new TextEncoder('utf-8').encode(text))
     },
     decode (text) {
-      return atob(text)
+      return new TextDecoder('utf-8').decode(Base64.decode(text))
     }
   }
 }
